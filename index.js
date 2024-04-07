1a980836b2146b3cf3f4e8d75369c5af95763188
Meshmanuu <meshanyangu@gmail.com>
2024-04-07 10:06:09 +0300

    labdone

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your code here
+//Add your code here
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        name: name,
+        email: email
+      })
+    })
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById('id').innerText = data.id;
+    })
+    .catch(error => {
+      document.getElementById('error').innerText = error.message;
+    });
+  }
